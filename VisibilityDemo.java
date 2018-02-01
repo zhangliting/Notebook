9acf297e60121678e417f998b38947f55fbb5dd0
zhangliting <1109156011@qq.com>
2018-02-01 14:50:33 +0800

    Create VisibilityDemo.java

---
+++ VisibilityDemo.java
@@ -0,0 +1,34 @@
+public class VisibilityDemo {
+    public static void main(String[] args) {
+        Runner runner = new Runner();
+        runner.start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //并不能使runner线程退出，因为可见性问题，就像是缓存。
+
+        runner.shutdown();
+        System.out.println("Shut down!");
+    }
+}
+
+class Runner extends Thread{
+    //volatile 可以解决可见性问题。
+    private boolean quit = false;
+
+    @Override
+    public void run() {
+        while(!quit){
+//            System.out.println("------working-----");
+        }
+        System.out.println("Done!");
+    }
+
+    public void shutdown(){
+        quit = true;
+    }
+}
+
+
